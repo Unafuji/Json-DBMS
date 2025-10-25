46b003b165c92042e3655753ffa492b56e743b36
Unafuji <unafuji.swdev@gmail.com>
2025-10-26 04:16:38 +0700

    s

--- src/main/preload.js
+++ src/main/preload.js
@@ -1,14 +1,7 @@
-// preload.js
-const { contextBridge, ipcRenderer } = require('electron');
+
+const { contextBridge } = require('electron');
 
 contextBridge.exposeInMainWorld('stream', {
-    startFile: (filePath, opts) => ipcRenderer.invoke('stream:file:start', filePath, opts),
-    startTest: () => ipcRenderer.invoke('stream:test'),
-    cancel: () => ipcRenderer.invoke('stream:cancel'),
-    onChunk: cb => ipcRenderer.on('stream:chunk', (_, c) => cb(c)),
-    onDone: cb => ipcRenderer.on('stream:done',  (_, s) => cb(s)),
-    onError: cb => ipcRenderer.on('stream:error', (_, e) => cb(e)),
-    credit: () => ipcRenderer.send('stream:credit'),
-    stop:   () => ipcRenderer.send('stream:stop'),
+
 });
 
